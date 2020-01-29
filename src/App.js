6b775e5985401b82b664f8d04f1079cd1ae19d82
Caleb O'Connell <caleb.oconnell@grandrounds.com>
2020-01-29 09:02:31 -0500

    adding a change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Adding content
         </a>
       </header>
     </div>
