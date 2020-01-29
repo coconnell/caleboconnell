b49e34c559ce4ba93fd178579152875a906e6edd
Caleb O'Connell <caleb.oconnell@grandrounds.com>
2020-01-29 09:10:17 -0500

    fixing

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Um.......
+          Changing
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Adding content
+          Learn React
         </a>
       </header>
     </div>
